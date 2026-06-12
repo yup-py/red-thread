5cf2d56d4b0b858b4c79768b71e5cf0c39e1c663
yuppy <bousserhaneayoub@gmail.com>
2026-06-12 16:11:36 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load your dataset
-df = pd.read_csv("shows+casts+platform+score+budget+genre\streaming_catalog.csv")
-
-# View unique values in the 'Platform' column
-unique_platforms = df['platform'].unique()
-print(unique_platforms)
